4a67a02750e71920d6687a4e16eb961a8e75ab97
aaronisraelflorescastro <156271174+aaronisraelflorescastro@users.noreply.github.com>
2024-02-14 20:54:16 -0600

    Add files via upload

---
+++ list.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", () => {
+    document.querySelector(".btn.btn-primary").addEventListener("click", event => {
+        var itemList = document.getElementById("my-list");
+        var template = document.getElementById("list-template");
+        var total = itemList.childElementCount + 1;
+        var clone = template.content.cloneNode(true);
+        clone.querySelector("[data-id='number']").textContent = `${total}`;
+        clone.querySelector("[data-id='ID']").textContent = "ID: ";
+        clone.querySelector("[data-id='Name']").textContent = "Name: Rick Sanchez";
+        clone.querySelector("[data-id='status']").textContent = "Status: Vivo";
+        clone.querySelector("[data-id='species']").textContent = "Species: Humano";
+        itemList.appendChild(clone);
+    });
+
+    document.querySelector(".btn.btn-light").addEventListener("click", event => {
+        var itemList = document.getElementById("my-list");
+        itemList.replaceChildren();
+    });
+});
