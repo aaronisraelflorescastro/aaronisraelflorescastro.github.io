408f5ead282d8acb573929301d51e16e25cbbd45
aaronisraelflorescastro <156271174+aaronisraelflorescastro@users.noreply.github.com>
2024-02-24 20:06:28 -0600

    Update script.js

--- reva/script.js
+++ reva/script.js
@@ -1,14 +1,8 @@
-//Mostrar los personajes sin filtrar 
-//tenemos que obtener todos los personajes llamando a la api 
-//renderizarlos en nuestro dom 
-//cuando cambien los filtros se tiene que hacer un llamado nuevo a la api 
-//se debe renderizar nuevamente
-
 const charactersEl = document.getElementById('characters');
 const nameFilterEl = document.getElementById('name-filter');
 const statusFilterEl = document.getElementById('status-filter');
 
-//tenemos que crear la funcion que haga el llamado a la api
+// tenemos que crear la funcion que haga el llamado a la api
 
 async function getCharacters (name, status){
 
@@ -27,47 +21,45 @@ async function getCharacters (name, status){
 
 
     const response = await fetch(url);
-    const data = await response.json();
-
-    console.log(data.results);
+    const data = await response.json(); 
 
     return data.results;
 
 }
 
-// la funcion que a renderizar los elementos dentro del don
+// la funcion que va a renderizar los elementos dentro del DOM
 
-async function displayCharacters(name, status){
+async function displayCharacters (name, status) {
+    
+    // Obtener los personajes filtrados
+    const characters = await getCharacters(name, status);
+    
+    charactersEl.innerHTML = '';
 
-// Obtener los personajes filtrados
-const characters = await getCharacters(name, status);
+    // renderizar los personajes
+    for( let character of characters ){
+        const card = document.createElement('div');
+        card.classList.add('character-card');
 
-charactersEl.innerHTML= '';
+        card.innerHTML = `
+            <img src="${character.image}" />
+            <h2> ${character.name} </h2>
+            <p> Status: ${character.status} </p>
+            <p> Especie: ${character.species} </p>
+        `;
 
-// renderizar los personajes
-for( let character of characters ){
-    const card = document.createElement('div');
-card.classList.add('character-card');
+        charactersEl.appendChild(card);
 
-card.innerHTML = `
-    <img src="${character.image}" />
-    <h2> ${character.name} </h2>
-    <p> Status: ${character.status} </p>
-    <p> Especie: ${character.species} </p>
-`;
-
-charactersEl.appendChild(card);
-
-}
+    }
 
 }
 
 displayCharacters();
 
-nameFilterEl.addEventListener('input', () =>  {
-    displayCharacters(nameFilterEl.value, statusFilterE.value );
+nameFilterEl.addEventListener('input', () => {
+    displayCharacters(nameFilterEl.value, statusFilterEl.value );
 });
 
-statusFilterEl.addEventListener('change', ()=> {
+statusFilterEl.addEventListener('change', () => {
     displayCharacters(nameFilterEl.value, statusFilterEl.value );
 });
